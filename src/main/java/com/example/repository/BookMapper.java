4fc412d7811a03ae6fab2fed07497912b0fe67a2
bitcocom <bitcocom@empas.com>
2024-01-25 18:55:50 +0900

    toy2

--- src/main/java/com/example/repository/BookMapper.java
+++ src/main/java/com/example/repository/BookMapper.java
@@ -2,14 +2,11 @@ package com.example.repository;
 
 import com.example.entity.BookDTO;
 import com.example.entity.UserDTO;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 //@Mapper
-@Repository
+//@Repository
 public interface BookMapper {
     public List<BookDTO> bookList();
     public int bookInsert(BookDTO book);
