4dc8a896e90d9aba15e2d80d2dd53a51d57eb882
bitcocom <bitcocom@empas.com>
2024-01-25 13:25:40 +0900

    toy2

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -1,42 +1,28 @@
 package com.example.controller;
 
 import com.example.entity.BookDTO;
-import com.example.repository.BookMyBatisDAO;
+import com.example.entity.UserDTO;
+import com.example.repository.BookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class BookController{
-     // 4개의 단위업무를 혼자서 처리....
-    // /list.do -> BookListController
-    // Spring WEB MVC
-    // /list.do -> BookController ---> Method로 연결
-    // /delete.do -> BookController ---> Method로 연결
-    // /register.do -> BookController ---> Method로 연결
-    // /registerPost.do -> BookController ---> Method로 연결
-   // @RequestMapping("/list.do") // HandlerMapping 이 동작을 한다.
-    public String list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+@Controller // Spring에서 POJO를 나타내기위해서사용하는 어노테이션
+public class BookController {   // new BookController() : Spring Container
 
-        BookMyBatisDAO dao=new BookMyBatisDAO();
-        List<BookDTO> list=dao.bookList();
-        req.setAttribute("list",list );
-
-        return "list";
-    }
-
-   // @RequestMapping("/delete.do")
-    public String delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        int num=Integer.parseInt(req.getParameter("num")); // 5
-
-        //BookDAO dao=new BookDAO();
-        BookMyBatisDAO dao=new BookMyBatisDAO();
-        int cnt=dao.bookDelete(num); // 저장 후에 -> 다시 리스트 보기로 이동 -> /list : redirect(리다이렉트)
-
-        return "redirect:list.do"; // JSP(X) -> /WEB-INF/views//shopping/list.jsp
-    }
-
+    @Autowired  // DI(의존성 주입)
+    private BookMapper mapper; // new SqlSessionFactoryBean()<--implements BookMapper
+       // http://localhost:8081/shopping/list
+     @RequestMapping("/list")
+     public String list(Model model){ // HttpServletRequest req
+         List<BookDTO> list=mapper.bookList();
+         model.addAttribute("list", list);
+         return "list"; // /WEB-INF/views/list.jsp <--- ${list}
+     }
 }
+
--- src/main/java/com/example/frontcontroller/ViewResolver.java
+++
@@ -1,7 +0,0 @@
-package com.example.frontcontroller;
-
-public class ViewResolver {
-    public static String makeView(String view){ // list
-        return "/WEB-INF/views/"+view+".jsp";
-    }
-}
